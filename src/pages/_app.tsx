c61cdde60c21df1eea81a223c4889ec167a43dae
Eoan O'Dea <odeaeoan@gmail.com>
2026-03-07 09:24:02 +0100

    Boilerplate, huskey, index & 404 implemented

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { AppCacheProvider } from "@mui/material-nextjs/v16-pagesRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "@/config/theme";
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+  return (
+    <AppCacheProvider {...props}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </AppCacheProvider>
+  );
+}
